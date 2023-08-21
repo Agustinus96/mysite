0b1748503ea0e9114fda34757326a0d1049a4c21
アグス・ティヌス <agusutinus@agusutinusus-MacBook-Pro.local>
2023-08-21 23:21:49 +0900

    new

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import About from "./about";
+
+export default function Page() {
+  return (
+    <div>
+      <About />
+      <h1 class="bg-transparent">Hello this is about page</h1>
+    </div>
+  );
+}
